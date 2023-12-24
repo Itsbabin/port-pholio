ab8d2cb7e17e07553284148d34e0741f26db3e19
Biswajit Mandal <itsbabin2004@gmail.com>
2023-12-25 00:18:08 +0530

    toggell style transition

--- src/component/sidenav.jsx
+++ src/component/sidenav.jsx
@@ -4,26 +4,22 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function Sidenav() {
-   const [height, setHeight] = useState("flex");
-   const [line1, setLine1] = useState(6);
-   const [line2, setLine2] = useState(18);
+   const [height, setHeight] = useState("0vw");
   return (<>
     <div onClick={() => {
-      height === "flex" ? setHeight("none") : setHeight("flex");
-      line1 === 6 ? setLine1(18) : setLine1(6);
-      line2 === 6 ? setLine2(18) : setLine2(6);
+      height === "0vw" ? setHeight("-100vw") : setHeight("0vw");
     }} className="hamburger">
     <svg viewBox="0 0 24 24" width="60" height="60" fill="none" stroke="#343334" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <line x1="3" y1="12" x2="21" y2="12" />
-    <line x1="3" y1="6" x2="21" y2={line1} />
-    <line x1="3" y1="18" x2="21" y2={line2} />
+    <line className="line2" x1="3" y1="12" x2="21" y2="12" />
+    <line className="line1" x1="3" y1="6" x2="21" y2="6" />
+    <line className="line3" x1="3" y1="18" x2="21" y2="18" />
 </svg>
     </div>
-    <div className="sidenav" style={{display: height}}> 
-        <Link className="nextlink" style={{height: height}} href={'/home'}> <div className="nav_item">  Home </div> </Link>
-        <Link className="nextlink" style={{height: height}} href={'/abot'}> <div className="nav_item">  About </div> </Link>
-        <Link className="nextlink" style={{height: height}} href={'/projects'}> <div className="nav_item">  Projects </div> </Link>
-        <Link className="nextlink" style={{height: height}} href={'/contact'}> <div className="nav_item">  Contact </div> </Link>
+    <div className="sidenav" style={{ marginLeft: height}}> 
+        <Link className="nextlink" href={'/home'}> <div className="nav_item">  Home </div> </Link>
+        <Link className="nextlink" href={'/abot'}> <div className="nav_item">  About </div> </Link>
+        <Link className="nextlink" href={'/projects'}> <div className="nav_item">  Projects </div> </Link>
+        <Link className="nextlink" href={'/contact'}> <div className="nav_item">  Contact </div> </Link>
     </div>
     </>
   )
