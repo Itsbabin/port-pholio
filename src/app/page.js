ec80628d8e3d20ff953b16b2e70044bbc7320af7
Biswajit Mandal <itsbabin2004@gmail.com>
2023-12-27 16:05:56 +0530

    contact me page ready

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,25 @@
-'use client';
-import Sidenav from '../component/sidenav'
-import Image from 'next/image';
-// import myphoto from '../../public/photos/myphoto.png'
-
+"use client";
+import Sidenav from "../component/sidenav";
+import Image from "next/image";
+import myphoto from "../../public/photos/newphoto.jpg";
 
 export default function Home() {
-  
   return (
-      <>
-      <Sidenav/>
-       <div className="container">
-        {/* <img src={myphoto.src} alt="Picture of the author" className="myphoto"></img>  */}
-       </div>
-      </>
-  )
+    <>
+      <Sidenav />
+      <div className="container">
+        <div className="image">
+          <Image className="photo" src={myphoto} width={350}></Image>
+        </div>
+        <div className="intro">
+          <h1 className="heading">
+           Hello !!
+          </h1> 
+          <h3>
+            Lets dive into world of webedev with me . My shelf Biswajit Mandal a intermidiate webdeveloper.
+          </h3>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/component/sidenav.jsx
+++ src/component/sidenav.jsx
@@ -4,22 +4,43 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function Sidenav() {
-   const [height, setHeight] = useState("0vw");
+   const [height, setHeight] = useState("-100vw");
+   const [area, setArea] = useState("false");
   return (<>
     <div onClick={() => {
+
       height === "0vw" ? setHeight("-100vw") : setHeight("0vw");
-    }} className="hamburger">
-    <svg viewBox="0 0 24 24" width="60" height="60" fill="none" stroke="#343334" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      area === "false" ? setArea("true") : setArea("false");
+
+    }} aria-expanded={area} className="hamburger">
+    <svg viewBox="0 0 24 24" width="60" height="60" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
     <line className="line2" x1="3" y1="12" x2="21" y2="12" />
     <line className="line1" x1="3" y1="6" x2="21" y2="6" />
     <line className="line3" x1="3" y1="18" x2="21" y2="18" />
 </svg>
     </div>
     <div className="sidenav" style={{ marginLeft: height}}> 
-        <Link className="nextlink" href={'/home'}> <div className="nav_item">  Home </div> </Link>
-        <Link className="nextlink" href={'/abot'}> <div className="nav_item">  About </div> </Link>
-        <Link className="nextlink" href={'/projects'}> <div className="nav_item">  Projects </div> </Link>
-        <Link className="nextlink" href={'/contact'}> <div className="nav_item">  Contact </div> </Link>
+        <Link className="nextlink" href={'/'}>
+          <div className="nav_item">
+             <div> Home </div>
+          </div>
+        </Link>
+        <Link className="nextlink" href={'/about'}>
+          <div className="nav_item">
+             <div> About </div>
+          </div>
+        </Link>
+        <Link className="nextlink" href={'/contact'}>
+          <div className="nav_item">
+             <div> Contact </div>
+          </div>
+        </Link>
+        <Link className="nextlink" href={'/projects'}>
+          <div className="nav_item">
+             <div> Projects </div>
+          </div>
+        </Link>
+       
     </div>
     </>
   )
