6653e2a9d9800e3b34e7f919c575619aa1fae788
Biswajit Mandal <itsbabin2004@gmail.com>
2023-12-28 11:08:24 +0530

    nav arrow success fully impiimented

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import Sidenav from "../component/sidenav";
 import Image from "next/image";
-import myphoto from "../../public/photos/newphoto.jpg";
+import myphoto from "../../public/photos/myphoto.png";
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="container">
         <div className="wraper">
           <div className="image">
-            <Image className="photo" src={myphoto} width={350}></Image>
+            <Image className="photo" src={myphoto} width={350} alt="author photo" priority={true} ></Image>
           </div>
           <div className="intro">
             <h1 className="heading">Hello !!</h1>
--- src/component/nevigate_arrow.jsx
+++ src/component/nevigate_arrow.jsx
@@ -1,16 +1,68 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { usePathname } from 'next/navigation'
+import Link from "next/link";
+
 
 export default function Navigate() {
+  const [path, setPath] = useState("");
+  const [navidata, setNavidata] = useState("");
+  const [path2, setPath2] = useState("");
+  const [navidata2, setNavidata2] = useState("");
+  const url = usePathname();
+
+  useEffect(() => {
+    if(url == "/"){
+      setPath("projects");
+      setPath2("skills");
+      setNavidata("Projects");
+      setNavidata2("Skills");
+    }
+    else if(url == "/skills"){
+      setPath("");
+      setPath2("about");
+      setNavidata("Home");
+      setNavidata2("About");
+    }
+    else if(url == "/about"){
+      setPath("skills");
+      setPath2("contact");
+      setNavidata("Skills");
+      setNavidata2("Contact");
+    }
+    else if(url == "/contact"){
+      setPath("about");
+      setPath2("projects");
+      setNavidata("About");
+      setNavidata2("Projects");
+    }
+    else if(url == "/projects"){
+      setPath("contact");
+      setPath2("");
+      setNavidata("Contact");
+      setNavidata2("Home");
+    }
+    else{
+      setPath("");
+      setPath2("");
+      setNavidata("Home");
+      setNavidata2("Home");
+    }
+  }, [url])
+  
+
+  
   return (
     <>
+      <Link className="nextlink" href={`/${path}`}>
       <div className="left">
         <svg
           viewBox="0 0 30 30"
           width="70"
-          height="70"
+          height="80"
           fill="none"
           stroke="#ffffff"
-          strokeWidth="2"
+          strokeWidth="3"
           strokeLinecap="round"
           strokeLinejoin="round"
         >
@@ -18,22 +70,27 @@ export default function Navigate() {
           <line className="up"   x1="3" y1="16" x2="20" y2="23" />
           <line className="down" x1="3" y1="16" x2="20" y2="8" />
         </svg>
+      <div className="navigate">{navidata}</div> 
       </div>
+      </Link>
+      <Link className="nextlink" href={`/${path2}`}>
       <div className="right">
+       <div className="navigate">{navidata2}</div> 
         <svg
           viewBox="0 0 30 30"
           width="70"
-          height="70"
+          height="80"
           fill="none"
           stroke="#ffffff"
-          strokeWidth="2"
+          strokeWidth="3"
           strokeLinecap="round"
           strokeLinejoin="round"
         >
-          <line className="up"    x1="3" y1="23" x2="21" y2="12" />
-          <line className="down"  x1="3" y1="1" x2="21" y2="12" />
+          <line className="down"    x1="3" y1="23" x2="20" y2="14" />
+          <line className="up"  x1="3" y1="6" x2="20" y2="14" />
         </svg>
       </div>
+        </Link>
     </>
   );
 }
